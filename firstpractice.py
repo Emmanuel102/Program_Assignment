9c98be3440f56ca208cb9d2a2bc05af1fa1492c1
Emmanuel102 <hardcorekdog103@gmail.com>
2021-06-22 18:54:26 -0500

    first commit

---
+++ firstpractice.py
@@ -0,0 +1,26 @@
+from typing import ForwardRef
+
+print("Hello World!!!")
+
+# step 1
+name = 'LeBron' # string
+age = 35
+salary = 37.44 # float
+plays_basketball = True #boolean
+jersey_number = '23' # string
+
+# step 2
+car_name = 'ford'
+x = 50
+my_first_name = 'Kurowei'
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
+
+
---
+++ list_prac.py
@@ -0,0 +1,44 @@
+city_names = ["Oakland", "Seattle", "Atlanta", "New York City", "Memphis", "Miami", "Los Angeles", "New Orleans"]
+#                0          1          2            3
+
+
+
+
+
+
+def printCityNames ():
+    counter = 0
+    for len(city_names)
+    while counter < len(city_names):
+        print(city_names[counter])
+        counter += 12
+        return "completed"
+
+
+        printCityNames(city_names)
+
+
+
+#[longer names ... shorter names]
+# getthe length of listitem1, listitem2
+check if len[list item 1] > len[next list item 2]
+move on
+else
+remove listitem
+put list item at end
+#return list!
+
+      
+#return new list 
+ 
+        def organize_cities(cities_list):
+            print(len(cities_list[0]))
+    for len(city_names)
+if(len(cities_list[counter])) > len(cities_list[counter + 1])):
+    print()
+    counter+=1
+    else:
+        cities_list.remove(city)
+        cities_list.append(city)
+        counter +=1
+organize_cities(c)
