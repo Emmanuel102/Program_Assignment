6290cff62aac7e40a2cf8178d048f55a19f6151a
Emmanuel102 <hardcorekdog103@gmail.com>
2021-06-28 15:53:33 -0500

    first commit

---
+++ dict_prac.py
@@ -0,0 +1,22 @@
+# step 2
+
+groceries = {'Chicken': 1.59, 'Beef': 1.99, 'Cheese': 1.00, 'Milk': 2.5 }
+# printgroceries
+
+chicken_price = groceries['Chicken']
+Beef_price = groceries['Beef']
+
+
+# step 3
+anime = {'Naruto': 2005, 
+        'Fullmetal Alchemist': 2004, 
+        'One Piece': 2003, 
+        'Jujitsu Kaisen': 2018}
+
+Naruto = anime ['Naruto']
+Fullmetal_Alchemist = anime['Fullmetal Alchemist']
+anime['Naruto'] = 2008
+
+print(anime)
+del anime['Naruto']
+print(anime)
